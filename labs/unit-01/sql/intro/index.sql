54033b192dc9c5d2d16b1ee1c6533479325d6bf1
dplaza <dplaza@gmx.com>
2022-04-28 21:04:06 +0200

    dev: finished the sql-intro lab

---
+++ labs/unit-01/sql/intro/index.sql
@@ -0,0 +1,50 @@
+-- sql-intro
+
+-- 1.0
+-- use sakila-pg;
+
+-- 2.0
+
+select * from actor;
+select * from film;
+select * from customer;
+
+-- 3.0
+select -- 2 --
+    *,
+    film.title
+        from 
+            film; -- 1st -- 
+
+
+-- 4.0 
+select -- 2nd -- 
+    distinct 
+        language.name,
+        language.language_id
+            from 
+                language -- 1st --
+                    order by 
+                        language.language_id; -- 3rd --
+
+-- 5.0
+
+-- 5.1
+select
+    distinct
+        count(store.store_id) as qty_stores
+            from
+                store;
+
+-- 5.2
+select
+    distinct
+        count(staff.store_id) as qty_staff
+            from
+                staff;
+
+-- 5.3
+select
+    staff.first_name
+        from
+            staff;
