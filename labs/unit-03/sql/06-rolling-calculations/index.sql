0f19df8fc26cf402346acc3131789893048a6bd1
dplaza <dplaza@gmx.com>
2022-05-27 19:18:37 +0200

    dev: labs done until sql-rolling-calculations

---
+++ labs/unit-03/sql/06-rolling-calculations/index.sql
@@ -0,0 +1,13 @@
+-- # Lab | SQL Rolling calculations
+
+
+-- 1. Get number of monthly active customers.
+
+
+-- 2. Active users in the previous month.
+
+
+-- 3. Percentage change in the number of active customers.
+
+
+-- 4. Retained customers every month.
