521e0e5afeb2d553798cc931664975798dc5916e
dplaza <dplaza@gmx.com>
2022-07-03 15:49:18 +0200

    dev: unit-04 lab-06 done

--- labs/unit-04/06-comparing-regression-models/utils/index.py
+++ labs/unit-04/06-comparing-regression-models/utils/index.py
@@ -1,5 +1,11 @@
 from pydoc import doc
-from .docs import *
+from typing import final
+# from .docs import *
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
 
 def split_text_on_uppercase(s, keep_contiguous=False):
     string_length = len(s)
@@ -32,5 +38,56 @@ def categorize_numeric(set):
     return set[discrete_subset], set[continuous_subset]
 
 
-for func in [categorize_numeric]:
-    func.__doc__ = eval(f"DOCS_{func.__name__}")
+def split_data(df, target):
+    df = df.dropna()
+    try:
+        X = df.drop([target], axis=1)
+        y = df[target]
+        return train_test_split(X, y)
+    except:
+        raise
+    
+
+def scale_data(sets, scaler):
+    lst_scaled_sets = []
+    try:
+        for set in sets:
+            set_scaled = scaler().fit_transform(set.select_dtypes(np.number))
+            set[set.select_dtypes(np.number).columns] = set_scaled
+            lst_scaled_sets.append(set)
+        return lst_scaled_sets
+    except ValueError as err:
+        print(err.message)
+
+
+def encode_data(scaled_sets, encoder):
+    lst_scaled_and_encoded_sets = []
+    try:
+        for scaled_set in scaled_sets:
+            cols_to_encode=scaled_set.select_dtypes(object).columns.values
+            enc = encoder(drop='first').fit(scaled_set[cols_to_encode])
+            oh_encoded_scaled_set = pd.DataFrame(enc.transform(scaled_set[cols_to_encode]).toarray())
+            oh_encoded_scaled_set= oh_encoded_scaled_set.set_index(scaled_set.index)
+            scaled_and_encoded_set = scaled_set.drop(scaled_set[cols_to_encode], axis=1).join(oh_encoded_scaled_set)
+            lst_scaled_and_encoded_sets.append(scaled_and_encoded_set)
+        return lst_scaled_and_encoded_sets
+    except ValueError as err:
+        print(err.message)
+    except TypeError as err:
+        print(err.message)
+    except:
+        raise
+        
+
+# for func in [categorize_numeric]:
+#     func.__doc__ = eval(f"DOCS_{func.__name__}")
+
+
+
+# X = [['Male', 1], ['Female', 3], ['Female', 2]]
+# Y = [['Male', 1], ['Female', 3], ['Female', 2]]
+# lst_sets = [X, Y]
+
+# X_e, Y_e = encode_data(lst_sets, OneHotEncoder)
+
+# print(X_e, Y_e)
